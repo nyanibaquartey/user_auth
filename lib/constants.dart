c8cf38e25eee2c6a827ff8bf43a315719a53b6e8
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-19 16:17:12 +0000

    Create textfield decoration constant

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: '',
+  hintStyle: TextStyle(color: Colors.black45),
+  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 18.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blue, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+);
