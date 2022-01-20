45c20ef9f2458a09d6b43d835bcb3e23b7c0ecee
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-20 17:26:11 +0000

    Edit border radius and content padding

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 const kTextFieldDecoration = InputDecoration(
   hintText: '',
   hintStyle: TextStyle(color: Colors.black45),
-  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 18.0),
+  contentPadding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 15.0),
   border: OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
   ),
   enabledBorder: OutlineInputBorder(
     borderSide: BorderSide(color: Colors.blue, width: 1.0),
-    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
   ),
   focusedBorder: OutlineInputBorder(
     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
