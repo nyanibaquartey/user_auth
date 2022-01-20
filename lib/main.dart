07dfcaeacbacb3027f6d629af131470619aad092
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-20 17:01:03 +0000

    Move class into a new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'constants.dart';
+import 'package:login_app/screens/login_page.dart';
 
 void main() {
   runApp(const LoginApp());
@@ -15,53 +15,7 @@ class LoginApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(title: 'Log In App'),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  final String title;
-  const SplashScreen({Key? key, required this.title}) : super(key: key);
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextField(
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
-                prefixIcon: const Icon(Icons.email),
-              ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            TextField(
-              obscureText: true,
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
-                prefixIcon: const Icon(
-                  Icons.password,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      home: const LoginPage(title: 'Log In App'),
     );
   }
 }
