9f86640a65060c9aaed95025df87700085e1ffc8
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-01-31 15:27:47 +0000

    Create welcome screen

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:login_app/screens/login_page.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Welcome to Greatly\'s App,',
+              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              height: 50.0,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 20.0),
+                    elevation: 3.0,
+                    side: const BorderSide(
+                        style: BorderStyle.solid,
+                        color: Colors.blue,
+                        width: 2.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  child: const Text('Register')),
+            ),
+            const SizedBox(
+              height: 21.0,
+            ),
+            SizedBox(
+              height: 50.0,
+              child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 20.0),
+                    elevation: 3.0,
+                    side: const BorderSide(
+                        style: BorderStyle.solid,
+                        color: Colors.blue,
+                        width: 2.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                  ),
+                  child: const Text('Log in')),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
