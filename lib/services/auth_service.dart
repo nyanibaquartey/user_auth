d6684b38efba87aab637f5d45f8655db71889606
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-02-01 10:29:22 +0000

    Add register method

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -11,7 +11,18 @@ class Authentication {
       {required String email, required String password}) async {
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
-      return 'Signed in';
+      return 'Log in successful';
+    } on FirebaseAuthException catch (e) {
+      return e.message.toString();
+    }
+  }
+
+  Future<String> register(
+      {required String email, required String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return 'Registered succesfully';
     } on FirebaseAuthException catch (e) {
       return e.message.toString();
     }
