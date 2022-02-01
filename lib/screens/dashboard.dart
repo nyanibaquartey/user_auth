419bed8b32ccae43246a6bed3bfb9b2d56780f2a
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-02-01 19:53:23 +0000

    Add dashboard screen

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: const Text(
+        'Welcome to Greatlys App',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.amberAccent,
+        ),
+      ),
+    );
+  }
+}
