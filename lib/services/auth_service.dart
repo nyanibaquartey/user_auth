bc3bbfe11433291e5983844027bcf16a74f292d0
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-02-02 14:14:18 +0000

    Change FirebaseAuth variable name

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,16 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 class Authentication {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseAuth _firebaseAuth;
 
-  //Authentication({this._auth});
+  Authentication(this._firebaseAuth);
 
-  Stream<User?> get authStateChanges => _auth.idTokenChanges();
+  Stream<User?> get authStateChanges => _firebaseAuth.idTokenChanges();
 
   Future<String> logIn(
       {required String email, required String password}) async {
     try {
-      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
       return 'Log in successful';
     } on FirebaseAuthException catch (e) {
       return e.message.toString();
@@ -20,7 +21,7 @@ class Authentication {
   Future<String> register(
       {required String email, required String password}) async {
     try {
-      await _auth.createUserWithEmailAndPassword(
+      await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
       return 'Registered succesfully';
     } on FirebaseAuthException catch (e) {
