51e00ba6efa8d6878b72bdf56982d5aad4211950
nananyanibaquartey <nyaniba.quartey@gmail.com>
2022-02-02 13:14:24 +0000

    Add Form, Change TextField to TextFormField

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -13,6 +13,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,60 +23,63 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 28.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Text(
-              'Welcome to Greatly\'s App,',
-              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(
-              'Log in to your account with email',
-              style: TextStyle(fontSize: 18.0),
-            ),
-            const SizedBox(
-              height: 25.0,
-            ),
-            TextField(
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your email',
-                prefixIcon: const Icon(Icons.email),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text(
+                'Welcome to Greatly\'s App,',
+                style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(
-              height: 21.0,
-            ),
-            TextField(
-              obscureText: true,
-              decoration: kTextFieldDecoration.copyWith(
-                hintText: 'Enter your password',
-                prefixIcon: const Icon(
-                  Icons.password,
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Text(
+                'Log in to your account with email',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              const SizedBox(
+                height: 25.0,
+              ),
+              TextFormField(
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                  prefixIcon: const Icon(Icons.email),
                 ),
               ),
-            ),
-            // const SizedBox(height: 21.0),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Forgot Password',
-                    style: TextStyle(color: Colors.blue),
-                  )),
-            ),
-            // const SizedBox(height: 21.0),
-            RoundedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, Dashboard.id);
-              },
-              title: 'Log in',
-            ),
-          ],
+              const SizedBox(
+                height: 21.0,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                  prefixIcon: const Icon(
+                    Icons.password,
+                  ),
+                ),
+              ),
+              // const SizedBox(height: 21.0),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Forgot Password',
+                      style: TextStyle(color: Colors.blue),
+                    )),
+              ),
+              // const SizedBox(height: 21.0),
+              RoundedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, Dashboard.id);
+                },
+                title: 'Log in',
+              ),
+            ],
+          ),
         ),
       ),
     );
